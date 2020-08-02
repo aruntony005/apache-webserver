39bdc0c9434efd6fdd46877ee9a3bc2acee2c332
aruntony005 <64554015+aruntony005@users.noreply.github.com>
2020-08-02 04:37:43 -0700

    Create webserver_install.sh

---
+++ webserver_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo sed -i 's/#baseurl=/baseurl=/g' /etc/yum.repos.d/CentOS-Base.repo
+sudo yum install httpd -y
+sudo sed -i 's/Listen 80/Listen 8080/g' /etc/httpd/conf/httpd.conf
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo echo '!!!!!  This is a sample !!!!!' >> /var/www/html/sample
