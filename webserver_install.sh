48fc75720933f0d9acd06afa6f2305a17b309e0b
aruntony005 <64554015+aruntony005@users.noreply.github.com>
2020-08-02 05:42:46 -0700

    Update webserver_install.sh

--- webserver_install.sh
+++ webserver_install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo sed -i 's/#baseurl=/baseurl=/g' /etc/yum.repos.d/CentOS-Base.repo
 sudo yum install httpd -y
 sudo sed -i 's/Listen 80/Listen 8080/g' /etc/httpd/conf/httpd.conf
 sudo systemctl start httpd
